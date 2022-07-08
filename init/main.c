ff604a5f31d25a23c1c7974b58127656eab0412b
Michael Bestas <mkbestas@lineageos.org>
2022-07-08 21:50:35 +0300

    random: Fix duplicate stack canary entropy init
    The original commit d55535232c3d moved stack canary entropy
    initialization, while the 4.9 backport missed removing the
    original initialization due to code differences.
    
    Fixes: 166a592cad36 ("random: move rand_initialize() earlier")
    Change-Id: I5b34f3102b4210f4553e54891bee02886dbf8794

--- init/main.c
+++ init/main.c
@@ -503,11 +503,6 @@ asmlinkage __visible void __init start_kernel(void)
 	page_address_init();
 	pr_notice("%s", linux_banner);
 	setup_arch(&command_line);
-	/*
-	 * Set up the the initial canary ASAP:
-	 */
-	add_latent_entropy();
-	boot_init_stack_canary();
 	mm_init_cpumask(&init_mm);
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
